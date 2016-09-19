a034ce9d8322d50594b03aceb59a99d5ee25888e
300435435 <300435435@PR-A313-08>
2016-09-19 19:25:15 -0400

    Add project files.

---
+++ ClassWebApp_2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassWebApp_2.Startup))]
+namespace ClassWebApp_2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
